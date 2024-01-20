bc5c74d5e948d0cdc45915c45aef57d8cefe0d57
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-20 11:53:25 -0500

    update script.js & readme.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JS New Years Day Sunday Checker
- Exercise: Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
+
+Exercise: Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
+*/
+
+// Define the start and end years
+// Loop through the years from startYear to endYear
+// Create a Date object for January 1 of the current year
+// Check if the day of the week for January 1 is Sunday (0 corresponds to Sunday)
+// If it's a Sunday, print or log the result
